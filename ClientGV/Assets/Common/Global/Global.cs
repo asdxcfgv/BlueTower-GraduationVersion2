9066df234ae7cb09a9492726133bceae5dbaf955
bulletrunner <2112094221@qq.com>
2026-02-03 23:07:20 +0800

    地图蓝图生成工具已提交

--- ClientGV/Assets/Common/Global/Global.cs
+++ ClientGV/Assets/Common/Global/Global.cs
@@ -4,8 +4,14 @@ using System.IO;
 using QFramework;
 using UnityEngine;
 
-public partial class Global : Architecture<Global>
+public  class Global : Architecture<Global>
 {
+    #region ROOM SETTINGS
+    public const float fadeInTime = 0.5f; // time to fade in the room
+    public const int maxChildCorridors = 3; // Max number of child corridors leading from a room. - maximum should be 3 although this is not recommended since it can cause the dungeon building to fail since the rooms are more likely to not fit together;
+    public const float doorUnlockDelay = 1f;
+    #endregion
+    
     protected override void Init()
     {
         
