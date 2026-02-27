20528c38e71ac00da87a4be10bd84220463fb2c9
bulletrunner <2112094221@qq.com>
2026-02-27 14:58:17 +0800

    暂存

--- ClientGV/Assets/Scripts/Enemies/Enemy.cs
+++ ClientGV/Assets/Scripts/Enemies/Enemy.cs
@@ -21,6 +21,7 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(IdleEvent))]
 [RequireComponent(typeof(Idle))]
 [RequireComponent(typeof(AnimateEnemy))]
+[RequireComponent(typeof(MaterializeEffect))]
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
@@ -48,6 +49,8 @@ public class Enemy : MonoBehaviour
     
     [HideInInspector] public IdleEvent idleEvent;
     
+    private MaterializeEffect materializeEffect;
+    
     private CircleCollider2D circleCollider2D;
     
     private PolygonCollider2D polygonCollider2D;
@@ -66,6 +69,7 @@ public class Enemy : MonoBehaviour
         enemyMovementAI = GetComponent<EnemyMovementAI>();
         movementToPositionEvent = GetComponent<MovementToPositionEvent>();
         idleEvent = GetComponent<IdleEvent>();
+        materializeEffect = GetComponent<MaterializeEffect>();
         circleCollider2D = GetComponent<CircleCollider2D>();
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
@@ -79,8 +83,22 @@ public class Enemy : MonoBehaviour
     {
         this.enemyDetails = enemyDetails;
 
+        SetEnemyMovementUpdateFrame(enemySpawnNumber);
+        
         SetEnemyAnimationSpeed();
         
+        // Materialise enemy
+        StartCoroutine(MaterializeEnemy());
+        
+    }
+    
+    /// <summary>
+    /// Set enemy movement update frame
+    /// </summary>
+    private void SetEnemyMovementUpdateFrame(int enemySpawnNumber)
+    {
+        // Set frame number that enemy should process it's updates
+        enemyMovementAI.SetUpdateFrameNumber(enemySpawnNumber % Global.targetFrameRateToSpreadPathfindingOver);
     }
     
     /// <summary>
@@ -90,5 +108,31 @@ public class Enemy : MonoBehaviour
     {
         // Set animator speed to match movement speed
         animator.speed = enemyMovementAI.moveSpeed / Global.baseSpeedForEnemyAnimations;
+    }
+    
+    private IEnumerator MaterializeEnemy()
+    {
+        // Disable collider, Movement AI and Weapon AI
+        EnemyEnable(false);
+
+        yield return StartCoroutine(materializeEffect.MaterializeRoutine(enemyDetails.enemyMaterializeShader, enemyDetails.enemyMaterializeColor, enemyDetails.enemyMaterializeTime, spriteRendererArray, enemyDetails.enemyStandardMaterial));
+
+        // Enable collider, Movement AI and Weapon AI
+        EnemyEnable(true);
+
+    }
+    
+    private void EnemyEnable(bool isEnabled)
+    {
+        // Enable/Disable colliders
+        circleCollider2D.enabled = isEnabled;
+        polygonCollider2D.enabled = isEnabled;
+
+        // Enable/Disable movement AI
+        enemyMovementAI.enabled = isEnabled;
+
+        // Enable / Disable Fire Weapon
+        fireWeapon.enabled = isEnabled;
+
     }
 }
